5e9f32bfafa6f5d80ffa0c3d6d5917196ff0dcd5
AdityaPatadiya <patadiyaaditya0079@gmail.com>
2025-02-04 08:25:06 +0530

    add matrix transformation.

--- Encryption/main.py
+++ Encryption/main.py
@@ -8,15 +8,43 @@ class Encryption:
 
     def conversion_into_matrices(self):
         hex_list = list(self.hex_string)  # Convert hex_string into a list of single characters
-        
+
         while len(hex_list) % 16 != 0:
             hex_list.append('0')  # Padding if necessary
-        
+
         self.matrices = [
             [hex_list[i + j:i + j + 4] for j in range(0, 16, 4)]
             for i in range(0, len(hex_list), 16)
         ]  # Convert into multiple 4x4 matrices
-    
+
+    def single_matrix_operations(self, matrices):
+        print(f"matrices: {matrices}")
+        # apply transpose of matrix
+        matrices = [list(map(list, zip(*matrix))) for matrix in matrices]
+        print(f"transpose matrices: {matrices}")
+
+        # Shift Rows
+        for matrix in matrices:
+            print(f"Original Matrix: {matrix}")
+            for i, row in enumerate(matrix):
+                print(f"matrix[{i}]: {row}")
+                if i == 1:
+                    matrix[i] = [row[-1]] + row[:-1]
+                elif i == 2:
+                    row[0], row[2] = row[2], row[0]
+                elif i == 3:
+                    matrix[i] = row[1:] + [row[0]]
+            print(f"Modified Matrix: {matrix}\n")
+
+        # matrix Multiplication
+        for matrix in matrices:
+            pass
+
+    def round_matrix_operation(self):
+        for _ in range(9):
+            self.single_matrix_operations(self.matrices)
+            print("=================================================")
+
 
 if __name__ == "__main__":
     encryption = Encryption()
@@ -24,4 +52,5 @@ if __name__ == "__main__":
 
     encryption.string_to_hex(plain_text)
     encryption.conversion_into_matrices()
-    print(encryption.matrices)
+    encryption.round_matrix_operation()
+    print(f"final matrices: {encryption.matrices}")
